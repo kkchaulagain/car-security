d8c38b0d699a8ce352ffe5b6ee842e89ea5097ed
kiran <kkchaulagain@gmail.com>
2023-06-30 16:39:13 +0545

    work

--- video_stream.py
+++ video_stream.py
@@ -3,16 +3,12 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-# Placeholder value for FOV (replace with the actual FOV of your camera module)
-FOV_DEGREES = 120.0
-
 def generate_frames():
     camera = cv2.VideoCapture(0)  # Open the camera
 
     # Print camera properties
     print("Camera Properties:")
     print("Resolution:", camera.get(cv2.CAP_PROP_FRAME_WIDTH), "x", camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print("FOV (degrees):", FOV_DEGREES)
     print("Framerate (fps):", camera.get(cv2.CAP_PROP_FPS))
 
     while True:
@@ -20,9 +16,6 @@ def generate_frames():
 
         if not success:
             break
-
-        # Resize the frame to fit the display
-        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
         # Convert the frame to JPEG format
         ret, buffer = cv2.imencode('.jpg', frame)
